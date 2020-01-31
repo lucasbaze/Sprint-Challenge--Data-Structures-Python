316237f2480737a74c8b7d3b989dde61a86a07e8
Lucas Bazemore <lucasabazemore@gmail.com>
2020-01-31 10:33:48 -0600

    names comments

--- names/names.py
+++ names/names.py
@@ -11,14 +11,19 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
+# Time Compelexity for this is O(n*m).
+
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
-# Using a dictionary... which isn't allowed? 
+
+# ATTEMPT 1
+
 # Runtime for this is 0.0076 at max
-# Time Complexity is O(max(n, m)) Space complexity is also O(max(n, m))
+# Time Complexity is O(max(n, m))
+# But I'm using a dictionary... which isn't allowed? 
 
     # names_dict = {}
     # for x in range(0, len(names_1)):
@@ -29,6 +34,9 @@ duplicates = []
     #         duplicates.append(name_2)
 
 # Need to use a different data structure that doesn't have O(1) search? 
+
+# ATTEMPT 2
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
